1e2073214e1a8e5a833a7182091f3b2c49017573
emre durgunlu <emredurgunlu@gmail.com>
2022-09-21 00:35:32 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "assets/assets/rive/1927-3970-hair-test.riv": "ee6b7b0fb8c181a4a1df5a2f67951e0d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "9b3f322865dbc642698073ab22af1571",
+"assets/NOTICES": "66cd227dc13e55d3162802be0540983f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f5638496985cc8cbfc88985801f85ffe",
-"/": "f5638496985cc8cbfc88985801f85ffe",
-"main.dart.js": "0ce3bb57d00089d2d748c86418330b03",
+"index.html": "bdfd3f23d29c44643135a5c0823efecf",
+"/": "bdfd3f23d29c44643135a5c0823efecf",
+"main.dart.js": "310bee7e18fb128a57bc0f79c8504dbc",
 "manifest.json": "ecefa2096116f3807786f249dc26400d",
 "version.json": "9e23208eff9bdc083f50e571bcf5621a"
 };
